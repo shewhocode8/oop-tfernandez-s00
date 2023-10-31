ed8a62b19043521d842aad502dab06985c01d524
shewhocode <shewhocode@gmail.com>
2023-10-31 21:38:10 +0800

    Add git commands in lecture notes

--- discussion/Main.java
+++ discussion/Main.java
@@ -3,3 +3,41 @@ class Main{
 		System.out.println("Mabuhay! Hello Pilipinas.");
 	}
 }
+
+/*
+	Git commands
+		- git init
+			- creates a git repository on the current directory
+		- git status
+			- allows us to see what changes have been staged, which haven't and what files aren't being tracked
+		- git add .
+			- it allows to prepare a file or set of files into staging area
+		- git commit -m "[message]"
+			- it allows us to save changes added from git add to its recorded history
+				ex. git commit -m "Initial commit"
+
+		- git config user.email "userEmail"
+			- it allows us to configure our email that is associated in Git
+				ex: git config user.email "shewhocode@gmail.com"
+
+			to check:
+				git config user.email
+
+		- git config user.name "shewhocode"
+			- it allows us to configure our username that is associated in Git
+				ex. git config user.name "shewhocode"
+
+			to check:
+				git config user.name
+
+
+
+
+
+
+
+
+
+
+
+*/
